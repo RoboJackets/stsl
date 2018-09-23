f51263cdff7d39582043655e95d62648a813ce0b
Mathew Barulic <matthew.barulic@gmail.com>
2018-09-22 22:58:02 -0400

    A bit of cleanup on the battery checking code.

--- STSL_Firmware/STSL_Firmware.ino
+++ STSL_Firmware/STSL_Firmware.ino
@@ -33,10 +33,17 @@ int right_b_channel = 4;
 int lift_a_channel = 5;
 int lift_b_channel = 6;
 
+int battery_pin = A13;
+double low_battery_voltage = 3.6;
+
 WiFiServer server(port);
 
 Adafruit_APDS9960 apds;
 bool apds_ready = false;
+
+double bitsToVolts(int bits) {
+  return (bits / 4096.0) * 7.4;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -50,17 +57,22 @@ void setup() {
   pinMode(us_trigger_pin, OUTPUT);
   pinMode(us_echo_pin, INPUT);
 
+  pinMode(battery_pin, INPUT);
+
   Serial.println("Pins ready.");
 
-  while(analogRead(A13) * 2 < 4000) {
-    for(int flash = 0; flash < 3; flash++) {
+  double battery_voltage = bitsToVolts(analogRead(battery_pin));
+  Serial.print("Battery: ");
+  Serial.print(battery_voltage);
+  Serial.println("v");
+  if(battery_voltage <= 3.6) {
+    while(true) {
       // ERROR FLASH!
       delay(100);
       digitalWrite(led, HIGH);
       delay(100);
       digitalWrite(led, LOW);
     }
-    delay(300);
   }
 
   Serial.println("Setting up ADPS 9960");
