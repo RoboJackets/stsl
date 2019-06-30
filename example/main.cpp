a010884e670539f0a9a981ce0fa272330e560d97
Matthew Barulic <matthew.barulic@gmail.com>
2019-06-30 02:03:42 +0000

    Cleans house in preperation for BBB STSL

--- example/main.cpp
+++ example/main.cpp
@@ -1,61 +1,10 @@
-#include "STSL/RJRobot.h"
 #include <iostream>
 
 using namespace std;
 
-/*
- * This example code demonstrates how to interact with all of the motors and sensors on Trainii.
- * This also doubles a system check program to debug malfunctioning robots.
- */
-
 int main() {
 
-    RJRobot robot(RobotType::REAL);
-
-    robot.SetDriveMotors(127, 127);
-    robot.Wait(1000ms);
-    robot.StopMotors();
-
-    robot.Wait(1000ms);
-
-    robot.SetLiftMotor(127);
-    robot.Wait(1000ms);
-    robot.SetLiftMotor(0);
-
-    robot.Wait(1000ms);
-
-    while (true) {
-        cout << "center line " << robot.GetLineValue(LineSensor::CENTER) << endl;
-
-        cout << "offset line " << robot.GetLineValue(LineSensor::OFFSET) << endl;
-
-        cout << "forward color ";
-        switch (robot.GetColor()) {
-            case Color::RED:     cout << "RED"; break;
-            case Color::BLUE:    cout << "BLUE"; break;
-            case Color::UNKNOWN: cout << "UNKNOWN"; break;
-            default: cout << "error";
-        }
-        cout << endl;
-
-        cout << "gesture ";
-        switch (robot.GetGesture()) {
-            case Gesture::RIGHT:  cout << "RIGHT"; break;
-            case Gesture::LEFT:   cout << "BLUE"; break;
-            case Gesture::UP:     cout << "UP"; break;
-            case Gesture::DOWN:   cout << "DOWN"; break;
-            case Gesture::NONE:   cout << "RIGHT"; break;
-            default: cout << "error";
-        }
-        cout << endl;
-
-        cout << "proximity " << robot.GetProximity() << endl;
-
-        cout << "ultrasonic " << robot.GetUltrasonicDistance() << endl;
-
-        robot.Wait(3s);
-        cout << endl;
-    }
+    cout << "example coming soon, to a theater near you." << endl;
 
     return 0;
 }
--- include/STSL/RJRobot.h
+++ include/STSL/RJRobot.h
@@ -1,77 +1,14 @@
 #ifndef TRAININGSUPPORTLIBRARY_RJROBOT_H
 #define TRAININGSUPPORTLIBRARY_RJROBOT_H
 
-#include <memory>
-#include <chrono>
-
-#ifdef _WIN32
-using socket_t = unsigned long long;
-#else
-using socket_t = int;
-#endif
-
-enum class LineSensor {
-    CENTER,
-    OFFSET
-};
-
-enum class Color {
-    RED,
-    BLUE,
-    UNKNOWN
-};
-
-enum class Gesture {
-    DOWN,
-    UP,
-    LEFT,
-    RIGHT,
-    NONE
-};
-
-enum RobotType {
-    REAL,
-    SIMULATOR
-};
-
 class RJRobot {
 
 public:
-    RJRobot(RobotType type);
+    RJRobot();
 
     ~RJRobot();
 
-    void SetDriveMotors(int leftPower, int rightPower);
-
-    void SetLiftMotor(int power);
-
-    void StopMotors();
-
-    double GetUltrasonicDistance();
-
-    double GetProximity();
-
-    Gesture GetGesture();
-
-    Color GetColor();
-
-    int GetLineValue(const LineSensor &sensor);
-
-    void Wait(std::chrono::microseconds duration);
-
 private:
-
-    socket_t socket_handle;
-
-    bool isValidSocketHandle(const socket_t &socket);
-
-    void sendCommand(const char* command);
-
-    std::string getResponse();
-
-    void handleError(const char *message);
-
-    void handleError(int retval, const char *message);
 
 };
 
