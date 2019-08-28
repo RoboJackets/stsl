91fc5a66487db3520f68b8e2d5059178f4b058b8
Matthew Barulic <matthew.barulic@gmail.com>
2019-08-28 12:36:44 -0700

    Adds support for line sensors.

--- src/RJRobot.cpp
+++ src/RJRobot.cpp
@@ -46,14 +46,26 @@ RJRobot::~RJRobot() {
 
 void RJRobot::setDriveMotors(double left_power, double right_power) {
     if(!battery_found) return;
-    if(rc_motor_set(1, left_power)) exit(-1);
-    if(rc_motor_set(2, right_power)) exit(-1);
+    if(rc_motor_set(LEFT_MOTOR_CHANNEL, left_power)) {
+        std::cerr << "ERROR: Failed to set left motor power\n";
+        exit(EXIT_FAILURE);
+    }
+    if(rc_motor_set(RIGHT_MOTOR_CHANNEL, right_power)) {
+        std::cerr << "ERROR: Failed to set right motor power\n";
+        exit(EXIT_FAILURE);
+    }
 }
 
 void RJRobot::stopMotors() {
     if(!battery_found) return;
-    if(rc_motor_free_spin(1)) exit(-1);
-    if(rc_motor_free_spin(2)) exit(-1);
+    if(rc_motor_free_spin(LEFT_MOTOR_CHANNEL)) {
+        std::cerr << "ERROR: Failed to free spin left motor\n";
+        exit(EXIT_FAILURE);
+    }
+    if(rc_motor_free_spin(RIGHT_MOTOR_CHANNEL)) {
+        std::cerr << "ERROR: Failed to free spin right motor\n";
+        exit(EXIT_FAILURE);
+    }
 }
 
 double RJRobot::getBatteryVoltage() {
@@ -62,6 +74,14 @@ double RJRobot::getBatteryVoltage() {
 
 const rc_mpu_data_t &RJRobot::getMPUData() {
     return mpu_data;
+}
+
+double RJRobot::getCenterLineSensor() {
+    return rc_adc_read_volt(CENTER_LINE_SENSOR_CHANNEL);
+}
+
+double RJRobot::getOffsetLineSensor() {
+    return rc_adc_read_volt(OFFSET_LINE_SENSOR_CHANNEL);
 }
 
 void RJRobot::checkForBattery() {
