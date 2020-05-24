1f6338e4ffa27c7568e209ced780455624d606cf
Matthew Barulic <matthew.barulic@gmail.com>
2020-05-24 01:01:18 -0700

    working through cross-compile failures.

--- deployment/cross_compilation/run_cross_compile.sh
+++ deployment/cross_compilation/run_cross_compile.sh
@@ -7,11 +7,17 @@ cd "$CROSS_COMPILE_DIR"
 
 mkdir -p cross_compile_ws/src
 
+cp defaults.yaml cross_compile_ws/
+
 vcs import cross_compile_ws/src < stsl.repos
 
-ros_cross_compile ./cross_compile_ws \
+cd cross_compile_ws
+
+ros_cross_compile \
 --arch armhf \
 --rosdistro eloquent \
 --os debian \
---custom-setup-script ./custom_setup_script.sh \
---colcon-defaults ./defaults.yaml
+--custom-setup-script ../custom_setup_script.sh \
+--colcon-defaults defaults.yaml \
+$(pwd)
+
