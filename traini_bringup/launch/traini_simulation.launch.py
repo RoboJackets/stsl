4a78bd1466f381ef356a333efab28600bb3c7020
Matthew Barulic <matthew.barulic@gmail.com>
2021-04-11 00:45:15 -0400

    Adds traini_description package.

--- traini_bringup/launch/traini_simulation.launch.py
+++ traini_bringup/launch/traini_simulation.launch.py
@@ -8,9 +8,9 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     gazebo_launch_path=os.sep.join([get_package_share_directory('traini_gazebo'),'launch','traini_gazebo.launch.py'])
+    robot_state_pub_path=os.sep.join([get_package_share_directory('traini_description'),'launch','robot_state_publisher.launch.py'])
 
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gazebo_launch_path)
-        )
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(gazebo_launch_path)),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(robot_state_pub_path))
     ])
---
+++ traini_description/launch/robot_state_publisher.launch.py
@@ -0,0 +1,33 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf_path = os.path.join(get_package_share_directory('traini_description'),'urdf','traini.urdf')
+
+    with open(urdf_path,'r') as urdf_file:
+        robot_description = urdf_file.read()
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                'robot_description': robot_description
+            }],
+            arguments=[urdf_path]
+        )
+    ])
