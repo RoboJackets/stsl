28a86b525a1cf6546069ba91090cfb8806d3cc83
Matthew Barulic <matthew.barulic@gmail.com>
2021-04-12 01:43:53 -0400

    Gets debian generation working with docker

--- deployment/generate_debs.sh
+++ deployment/generate_debs.sh
@@ -2,40 +2,7 @@
 
 set -e
 
-packages_to_skip="traini_onboard_interface"
-
-function install_rosdep_keys_file(){
-    rosdep_keys_path="$(pwd)/rosdep.yaml"
-    sudo echo "yaml file://$rosdep_keys_path" > /etc/ros/rosdep/sources.list.d/50-stsl-packages.list
-}
-
-function build_package(){
-    local package_path=$1
-    pushd $package_path > /dev/null
-    bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy
-    fakeroot debian/rules binary
-    popd > /dev/null
-}
-
-function install_package(){
-    local package_path=$1
-    local package_name=$(colcon list -n --paths $package_path)
-    local deb_style_package_name=${package_name//_/-}
-    local deb_path=$(ls ../ros-foxy-$deb_style_package_name*)
-    echo "Installing $deb_path"
-    sudo dpkg -i $deb_path
-}
-
-# Install needed tools
-sudo apt install python3-bloom fakeroot dpkg-dev debhelper
-
-install_rosdep_keys_file
-
-# Iterate through packages in topological order
-package_paths=$(colcon list -t -p --paths .. --packages-skip $packages_to_skip)
-
-for package_path in $package_paths; do
-    echo "\n\nBuilding $package_path ...\n"
-    build_package $package_path
-    install_package $package_path
-done
+docker build -t stsl_build -f Dockerfile ..
+docker create -ti --name stsl_build_temp stsl_build bash
+docker cp stsl_build_temp:/usr/src/workspace/src/stsl/debians.tar.gz .
+docker rm -f stsl_build_temp
---
+++ deployment/generate_debs_internal.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+
+set -e
+
+packages_to_skip="traini_onboard_interface"
+
+function install_rosdep_keys_file(){
+    rosdep_keys_path="$(pwd)/rosdep.yaml"
+    sudo echo "yaml file://$rosdep_keys_path" > /etc/ros/rosdep/sources.list.d/50-stsl-packages.list
+    rosdep update
+}
+
+function build_package(){
+    local package_path=$1
+    pushd $package_path > /dev/null
+    bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy
+    fakeroot debian/rules binary
+    popd > /dev/null
+}
+
+function install_package(){
+    local package_path=$1
+    local package_name=$(colcon list -n --paths $package_path)
+    local deb_style_package_name=${package_name//_/-}
+    local deb_path=$(ls ../ros-foxy-$deb_style_package_name*)
+    echo "Installing $deb_path"
+    sudo dpkg -i $deb_path
+}
+
+install_rosdep_keys_file
+
+source /opt/ros/foxy/setup.bash
+
+package_paths=$(colcon list -t -p --paths ../* --packages-skip $packages_to_skip)
+
+for package_path in $package_paths; do
+    echo "\n\nBuilding $package_path ...\n"
+    build_package $package_path
+    install_package $package_path
+done
+
+cd ..
+tar -czvf debians.tar.gz *.deb
