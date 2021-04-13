b3c6a893cc36d171fdaf66b02b723da049340e41
Matthew Barulic <matthew.barulic@gmail.com>
2021-04-12 23:22:37 -0400

    Cleans up urdf display launch file.

--- traini_description/launch/display.launch.py
+++ traini_description/launch/display.launch.py
@@ -14,24 +14,13 @@ def generate_launch_description():
         executable='robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
-    )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    )
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
+        on_exit=launch.actions.Shutdown()
     )
 
     return launch.LaunchDescription([
@@ -41,8 +30,6 @@ def generate_launch_description():
                                              description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                              description='Absolute path to rviz config file'),
-        joint_state_publisher_node,
-        joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node
     ])
