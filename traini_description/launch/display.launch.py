d13bf2214a1d384ea879c284f9b89848fafc3806
Matthew Barulic <matthew.barulic@gmail.com>
2021-04-25 21:32:57 -0400

    Adds linting to traini_description.

--- traini_description/launch/display.launch.py
+++ traini_description/launch/display.launch.py
@@ -1,3 +1,23 @@
+# Copyright 2021 RoboJackets
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import launch
 from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
@@ -5,14 +25,16 @@ import os
 
 
 def generate_launch_description():
-    pkg_share = launch_ros.substitutions.FindPackageShare(package='traini_description').find('traini_description')
+    pkg_share = launch_ros.substitutions.FindPackageShare(
+        package='traini_description').find('traini_description')
     default_model_path = os.path.join(pkg_share, 'urdf/traini.urdf')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf.rviz')
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'robot_description': Command(
+            ['xacro ', LaunchConfiguration('model')])}]
     )
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
@@ -25,10 +47,12 @@ def generate_launch_description():
 
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                             description='Flag to enable joint_state_publisher_gui'),
+                                             description='Flag to enable joint_state_publisher_gui'
+                                             ),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                              description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+        launch.actions.DeclareLaunchArgument(name='rvizconfig',
+                                             default_value=default_rviz_config_path,
                                              description='Absolute path to rviz config file'),
         robot_state_publisher_node,
         rviz_node
