109dddd5fe1dfd0f0da2747ed9f2c78e82a7549a
Matthew Barulic <matthew.barulic@gmail.com>
2021-04-25 21:23:34 -0400

    Enables linting on traini_bringup.

--- traini_bringup/launch/tag_detector.launch.py
+++ traini_bringup/launch/tag_detector.launch.py
@@ -1,7 +1,26 @@
-import os
-import launch
+# Copyright 2021 RoboJackets
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
+
 
 def generate_launch_description():
     return LaunchDescription([
@@ -10,7 +29,7 @@ def generate_launch_description():
             executable='aruco_tag_detection_node',
             output='screen',
             parameters=[
-                {'tag_size':0.2}
+                {'tag_size': 0.2}
             ]
         )
     ])
