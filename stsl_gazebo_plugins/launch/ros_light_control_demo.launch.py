405e2af8b69c17a9ecb98356f3579e126f818747
Matthew Barulic <matthew.barulic@gmail.com>
2021-04-25 21:18:38 -0400

    Enables linting on stsl_gazebo_plugins

--- stsl_gazebo_plugins/launch/ros_light_control_demo.launch.py
+++ stsl_gazebo_plugins/launch/ros_light_control_demo.launch.py
@@ -1,3 +1,23 @@
+# Copyright 2021 RoboJackets
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
@@ -5,21 +25,26 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 from ament_index_python.packages import get_package_prefix
 
-def generate_launch_description():
-    test_world_path=os.sep.join([get_package_share_directory('stsl_gazebo_plugins'),'worlds','ros_light_control_demo_world.sdf'])
-    gazebo_launch_path=os.sep.join([get_package_share_directory('gazebo_ros'),'launch','gazebo.launch.py'])
 
-    stsl_gazebo_plugin_path=os.sep.join([get_package_prefix('stsl_gazebo_plugins'),'lib'])
-    env_gazebo_plugin_path=os.pathsep.join([stsl_gazebo_plugin_path, os.getenv('GAZEBO_PLUGIN_PATH','')])
+def generate_launch_description():
+    test_world_path = os.sep.join([get_package_share_directory(
+        'stsl_gazebo_plugins'), 'worlds', 'ros_light_control_demo_world.sdf'])
+    gazebo_launch_path = os.sep.join(
+        [get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'])
+
+    stsl_gazebo_plugin_path = os.sep.join(
+        [get_package_prefix('stsl_gazebo_plugins'), 'lib'])
+    env_gazebo_plugin_path = os.pathsep.join(
+        [stsl_gazebo_plugin_path, os.getenv('GAZEBO_PLUGIN_PATH', '')])
 
     return LaunchDescription([
         SetEnvironmentVariable('GAZEBO_PLUGIN_PATH', env_gazebo_plugin_path),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(gazebo_launch_path),
             launch_arguments={
-                'world':test_world_path,
-                'verbose':'true',
-                'gui_required':'true'
-                }.items()
+                'world': test_world_path,
+                'verbose': 'true',
+                'gui_required': 'true'
+            }.items()
         )
     ])
--- stsl_gazebo_plugins/src/RosLightControlPlugin.cpp
+++ stsl_gazebo_plugins/src/RosLightControlPlugin.cpp
@@ -1,4 +1,23 @@
-#include <functional>
+// Copyright 2021 RoboJackets
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo_ros/node.hpp>
@@ -6,193 +25,197 @@
 #include <ignition/math/Vector3.hh>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/bool.hpp>
+#include <functional>
+#include <string>
 
 namespace stsl_gazebo_plugins
 {
-
 gazebo::physics::LinkPtr findLinkForLight(
-    const gazebo::physics::ModelPtr& model,
-    const std::string& light_name, 
-    const std::string& link_name)
+  const gazebo::physics::ModelPtr & model,
+  const std::string & light_name,
+  const std::string & link_name)
 {
-    auto child_link = model->GetChildLink(link_name);
-    if(child_link && child_link->GetSDF()->HasElement("light"))
-    {
-        auto sdf_light = child_link->GetSDF()->GetElement("light");
-        while(sdf_light)
-        {
-            if(sdf_light->Get<std::string>("name") == light_name)
-            {
-                return child_link;
-            }
-            sdf_light = sdf_light->GetNextElement("light");
-        }
+  auto child_link = model->GetChildLink(link_name);
+  if (child_link && child_link->GetSDF()->HasElement("light")) {
+    auto sdf_light = child_link->GetSDF()->GetElement("light");
+    while (sdf_light) {
+      if (sdf_light->Get<std::string>("name") == light_name) {
+        return child_link;
+      }
+      sdf_light = sdf_light->GetNextElement("light");
     }
-    for(auto nested_model : model->NestedModels())
-    {
-        auto found_link = findLinkForLight(nested_model, light_name, link_name);
-        if(found_link)
-            return found_link;
+  }
+  for (auto nested_model : model->NestedModels()) {
+    auto found_link = findLinkForLight(nested_model, light_name, link_name);
+    if (found_link) {
+      return found_link;
     }
-    return nullptr;
+  }
+  return nullptr;
 }
 
-double getLightRange(const gazebo::physics::LinkPtr& link, const std::string& light_name)
+double getLightRange(const gazebo::physics::LinkPtr & link, const std::string & light_name)
 {
-    if(!link->GetSDF()->HasElement("light"))
-    {
-        gzerr << "Link has no <light> element!" << std::endl;
-        return 0.0;
-    }
-    auto sdf_light = link->GetSDF()->GetElement("light");
-    while(sdf_light)
-    {
-        if(sdf_light->Get<std::string>("name") == light_name)
-        {
-            return sdf_light->GetElement("attenuation")->Get<double>("range");
-        }
-        sdf_light = sdf_light->GetNextElement("light");
-    }
-    gzerr << "Link has no <light> element with name " << light_name << std::endl;
+  if (!link->GetSDF()->HasElement("light")) {
+    gzerr << "Link has no <light> element!" << std::endl;
     return 0.0;
+  }
+  auto sdf_light = link->GetSDF()->GetElement("light");
+  while (sdf_light) {
+    if (sdf_light->Get<std::string>("name") == light_name) {
+      return sdf_light->GetElement("attenuation")->Get<double>("range");
+    }
+    sdf_light = sdf_light->GetNextElement("light");
+  }
+  gzerr << "Link has no <light> element with name " << light_name << std::endl;
+  return 0.0;
 }
 
-gazebo::msgs::Visual getVisualProperties(const gazebo::physics::LinkPtr& link, const std::string& name)
+gazebo::msgs::Visual getVisualProperties(
+  const gazebo::physics::LinkPtr & link,
+  const std::string & name)
 {
-    gazebo::msgs::Link link_msg;
-    link->FillMsg(link_msg);
-    const auto& visuals = link_msg.visual();
-    const auto expected_visual_name = link->GetScopedName() + "::" + name;
-    auto found_iter = std::find_if(visuals.begin(), visuals.end(), [&expected_visual_name](const auto& visual){
-        return visual.name() == expected_visual_name;
+  gazebo::msgs::Link link_msg;
+  link->FillMsg(link_msg);
+  const auto & visuals = link_msg.visual();
+  const auto expected_visual_name = link->GetScopedName() + "::" + name;
+  auto found_iter =
+    std::find_if(
+    visuals.begin(), visuals.end(), [&expected_visual_name](const auto & visual) {
+      return visual.name() == expected_visual_name;
     });
-    if(found_iter == visuals.end())
-    {
-        gzerr << "Could not find visual named " << name << std::endl;
-        return {};
-    }
-    return *found_iter;
+  if (found_iter == visuals.end()) {
+    gzerr << "Could not find visual named " << name << std::endl;
+    return {};
+  }
+  return *found_iter;
 }
 
 class RosLightControlPlugin : public gazebo::ModelPlugin
 {
 public:
-
-    void Load(gazebo::physics::ModelPtr parent, sdf::ElementPtr sdf) override
-    {
-        if(!sdf->HasElement("light_id"))
-        {
-            gzerr << "Parameter <light_id> is missing." << std::endl;
-        }
-
-        if(!sdf->HasElement("visual_name"))
-        {
-            gzerr << "Parameter <visual_name> is missing." << std::endl;
-        }
-
-        const auto light_id = sdf->Get<std::string>("light_id");
-        const auto visual_name = sdf->Get<std::string>("visual_name");
-
-        const int sep_pos = light_id.rfind("/");
-        const auto light_name = light_id.substr(sep_pos+1);
-        const auto link_name = light_id.substr(0, sep_pos);
-
-        const auto link = findLinkForLight(parent, light_name, link_name);
-
-        const auto visual_props = getVisualProperties(link, visual_name);
-
-        light_on_range_ = getLightRange(link, light_name);
-        light_off_transparency_ = visual_props.transparency();
-        light_on_emissive_color_ = gazebo::msgs::Convert(visual_props.material().emissive());
-
-        gazebo_node_ = boost::make_shared<gazebo::transport::Node>();
-        gazebo_node_->Init();
-
-        light_publisher_ = gazebo_node_->Advertise<gazebo::msgs::Light>("~/light/modify");
-        light_publisher_->WaitForConnection();
-
-        light_msg_.set_name(link->GetScopedName() + "::" + light_name);
-
-        visual_publisher_ = gazebo_node_->Advertise<gazebo::msgs::Visual>("~/visual");
-
-        visual_msg_.set_name(link->GetScopedName() + "::" + visual_name);
-        visual_msg_.set_parent_name(link->GetScopedName());
-        uint32_t visual_id;
-        link->VisualId(visual_name, visual_id);
-        visual_msg_.set_id(visual_id);
-
-        ros_node_ = gazebo_ros::Node::Get(sdf);
-
-        light_command_sub_ = ros_node_->create_subscription<std_msgs::msg::Bool>("/light_control/" + light_name, 1, std::bind(&RosLightControlPlugin::onCommandMsg, this, std::placeholders::_1));
-
-        update_connection_ = gazebo::event::Events::ConnectWorldUpdateBegin(std::bind(&RosLightControlPlugin::OnUpdate, this));
+  void Load(gazebo::physics::ModelPtr parent, sdf::ElementPtr sdf) override
+  {
+    if (!sdf->HasElement("light_id")) {
+      gzerr << "Parameter <light_id> is missing." << std::endl;
     }
 
-    void OnUpdate()
-    {
-        std::lock_guard<std::mutex> lock(state_mutex_);
-
-        if(!commanded_state_changed_)
-            return;
-
-        if(commanded_light_state_)
-            turnLightOn();
-        else
-            turnLightOff();
-
-        commanded_state_changed_ = false;
+    if (!sdf->HasElement("visual_name")) {
+      gzerr << "Parameter <visual_name> is missing." << std::endl;
     }
+
+    const auto light_id = sdf->Get<std::string>("light_id");
+    const auto visual_name = sdf->Get<std::string>("visual_name");
+
+    const int sep_pos = light_id.rfind("/");
+    const auto light_name = light_id.substr(sep_pos + 1);
+    const auto link_name = light_id.substr(0, sep_pos);
+
+    const auto link = findLinkForLight(parent, light_name, link_name);
+
+    const auto visual_props = getVisualProperties(link, visual_name);
+
+    light_on_range_ = getLightRange(link, light_name);
+    light_off_transparency_ = visual_props.transparency();
+    light_on_emissive_color_ = gazebo::msgs::Convert(visual_props.material().emissive());
+
+    gazebo_node_ = boost::make_shared<gazebo::transport::Node>();
+    gazebo_node_->Init();
+
+    light_publisher_ = gazebo_node_->Advertise<gazebo::msgs::Light>("~/light/modify");
+    light_publisher_->WaitForConnection();
+
+    light_msg_.set_name(link->GetScopedName() + "::" + light_name);
+
+    visual_publisher_ = gazebo_node_->Advertise<gazebo::msgs::Visual>("~/visual");
+
+    visual_msg_.set_name(link->GetScopedName() + "::" + visual_name);
+    visual_msg_.set_parent_name(link->GetScopedName());
+    uint32_t visual_id;
+    link->VisualId(visual_name, visual_id);
+    visual_msg_.set_id(visual_id);
+
+    ros_node_ = gazebo_ros::Node::Get(sdf);
+
+    light_command_sub_ = ros_node_->create_subscription<std_msgs::msg::Bool>(
+      "/light_control/" + light_name, 1,
+      std::bind(&RosLightControlPlugin::onCommandMsg, this, std::placeholders::_1));
+
+    update_connection_ = gazebo::event::Events::ConnectWorldUpdateBegin(
+      std::bind(&RosLightControlPlugin::OnUpdate, this));
+  }
+
+  void OnUpdate()
+  {
+    std::lock_guard<std::mutex> lock(state_mutex_);
+
+    if (!commanded_state_changed_) {
+      return;
+    }
+
+    if (commanded_light_state_) {
+      turnLightOn();
+    } else {
+      turnLightOff();
+    }
+
+    commanded_state_changed_ = false;
+  }
 
 private:
-    gazebo::event::ConnectionPtr update_connection_;
+  gazebo::event::ConnectionPtr update_connection_;
 
-    gazebo::transport::NodePtr gazebo_node_;
-    gazebo::transport::PublisherPtr light_publisher_;
-    gazebo::transport::PublisherPtr visual_publisher_;
+  gazebo::transport::NodePtr gazebo_node_;
+  gazebo::transport::PublisherPtr light_publisher_;
+  gazebo::transport::PublisherPtr visual_publisher_;
 
-    gazebo::msgs::Light light_msg_;
-    gazebo::msgs::Visual visual_msg_;
+  gazebo::msgs::Light light_msg_;
+  gazebo::msgs::Visual visual_msg_;
 
-    gazebo_ros::Node::SharedPtr ros_node_;
+  gazebo_ros::Node::SharedPtr ros_node_;
 
-    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr light_command_sub_;
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr light_command_sub_;
 
-    std::mutex state_mutex_;
-    bool commanded_light_state_{false};
-    bool commanded_state_changed_{true};
+  std::mutex state_mutex_;
+  bool commanded_light_state_{false};
+  bool commanded_state_changed_{true};
 
-    double light_on_range_{0.0};
-    ignition::math::Color light_on_emissive_color_;
-    double light_off_transparency_{0.2};
+  double light_on_range_{0.0};
+  ignition::math::Color light_on_emissive_color_;
+  double light_off_transparency_{0.2};
 
-    void onCommandMsg(const std_msgs::msg::Bool::SharedPtr msg)
-    {
-        std::lock_guard<std::mutex> lock(state_mutex_);
-        commanded_state_changed_ = commanded_light_state_ != msg->data;
-        commanded_light_state_ = msg->data;
-    }
+  void onCommandMsg(const std_msgs::msg::Bool::SharedPtr msg)
+  {
+    std::lock_guard<std::mutex> lock(state_mutex_);
+    commanded_state_changed_ = commanded_light_state_ != msg->data;
+    commanded_light_state_ = msg->data;
+  }
 
-    void turnLightOn()
-    {
-        light_msg_.set_range(light_on_range_);
-        light_publisher_->Publish(light_msg_);
+  void turnLightOn()
+  {
+    light_msg_.set_range(light_on_range_);
+    light_publisher_->Publish(light_msg_);
 
-        visual_msg_.set_transparency(0.0);
-        gazebo::msgs::Set(visual_msg_.mutable_material()->mutable_emissive(), light_on_emissive_color_);
-        visual_publisher_->Publish(visual_msg_);
-    }
+    visual_msg_.set_transparency(0.0);
+    gazebo::msgs::Set(
+      visual_msg_.mutable_material()->mutable_emissive(),
+      light_on_emissive_color_);
+    visual_publisher_->Publish(visual_msg_);
+  }
 
-    void turnLightOff()
-    {
-        light_msg_.set_range(0.0);
-        light_publisher_->Publish(light_msg_);
+  void turnLightOff()
+  {
+    light_msg_.set_range(0.0);
+    light_publisher_->Publish(light_msg_);
 
-        visual_msg_.set_transparency(light_off_transparency_);
-        gazebo::msgs::Set(visual_msg_.mutable_material()->mutable_emissive(), ignition::math::Color(0,0,0));
-        visual_publisher_->Publish(visual_msg_);
-    }
+    visual_msg_.set_transparency(light_off_transparency_);
+    gazebo::msgs::Set(
+      visual_msg_.mutable_material()->mutable_emissive(),
+      ignition::math::Color(0, 0, 0));
+    visual_publisher_->Publish(visual_msg_);
+  }
 };
 
 GZ_REGISTER_MODEL_PLUGIN(RosLightControlPlugin)
-    
-} // namespace stsl_gazebo_plugins
+
+}  // namespace stsl_gazebo_plugins
