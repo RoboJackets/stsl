240b30a6c0cd10f1537bc669c0d6b6bb4ec36ec6
Matthew Barulic <matthew.barulic@gmail.com>
2022-01-08 15:17:51 -0500

    Gets arm64 debian package generation working.

--- deployment/package_generation/generate_debs.sh
+++ deployment/package_generation/generate_debs.sh
@@ -2,7 +2,21 @@
 
 set -e
 
-docker build --no-cache -t stsl_build -f Dockerfile ../..
+# amd64
+
+docker build --no-cache -t stsl_build -f Dockerfile_amd64 ../..
 docker create -ti --name stsl_build_temp stsl_build bash
-docker cp stsl_build_temp:/usr/src/workspace/src/stsl/debians.tar.gz .
+docker cp stsl_build_temp:/usr/src/workspace/src/stsl/debians.tar.gz ./debians_amd64.tar.gz
 docker rm -f stsl_build_temp
+
+# arm64
+
+docker build --no-cache -t stsl_build -f Dockerfile_arm64 ../..
+docker create -ti --name stsl_build_temp stsl_build bash
+docker cp stsl_build_temp:/usr/src/workspace/src/stsl/debians.tar.gz ./debians_arm64.tar.gz
+docker rm -f stsl_build_temp
+
+COLOR_GREEN='\033[0;32m'
+COLOR_AUTO='\033[0m'
+
+echo -e "\n${COLOR_GREEN}Package generation complete!${COLOR_AUTO}"
