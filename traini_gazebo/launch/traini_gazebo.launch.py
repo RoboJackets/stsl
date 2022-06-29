09e6e017bdad5acd0396acbcc4c5547f048213bc
Matthew Barulic <matthew.barulic@gmail.com>
2022-06-28 21:02:07 -0400

    Fixes linting failures in launch file.

--- traini_gazebo/launch/traini_gazebo.launch.py
+++ traini_gazebo/launch/traini_gazebo.launch.py
@@ -18,37 +18,41 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import os
-import launch
 from launch import LaunchDescription
-from launch.event_handlers import OnShutdown
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import (AppendEnvironmentVariable, DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable,
-                            RegisterEventHandler, ExecuteProcess)
+from launch.actions import (AppendEnvironmentVariable, DeclareLaunchArgument,
+                            IncludeLaunchDescription, SetEnvironmentVariable)
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare, FindPackagePrefix
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
     return LaunchDescription([
-        AppendEnvironmentVariable('GAZEBO_MODEL_PATH', PathJoinSubstitution([FindPackageShare('traini_gazebo'), 'models'])),
-        AppendEnvironmentVariable('GAZEBO_PLUGIN_PATH', PathJoinSubstitution([FindPackagePrefix('stsl_gazebo_plugins'), 'lib'])),
+        AppendEnvironmentVariable('GAZEBO_MODEL_PATH', PathJoinSubstitution(
+            [FindPackageShare('traini_gazebo'), 'models'])),
+        AppendEnvironmentVariable('GAZEBO_PLUGIN_PATH', PathJoinSubstitution(
+            [FindPackagePrefix('stsl_gazebo_plugins'), 'lib'])),
         # Disable trying to connect to online model database
         SetEnvironmentVariable('GAZEBO_MODEL_DATABASE_URI', ''),
-        DeclareLaunchArgument('verbose', default_value='false', choices=['false','true']),
-        DeclareLaunchArgument('gui', default_value='true', choices=['false','true']),
+        DeclareLaunchArgument(
+            'verbose', default_value='false', choices=['false', 'true']),
+        DeclareLaunchArgument('gui', default_value='true',
+                              choices=['false', 'true']),
         IncludeLaunchDescription(
-            # TODO(barulicm) Copying the gazebo_ros launch files into this directory is a temporary way to get the params_file feature before it's been released
-            PythonLaunchDescriptionSource(PathJoinSubstitution([FindPackageShare('traini_gazebo'),'launch','gazebo.launch.py'])),
+            # TODO(barulicm) Copying the gazebo_ros launch files into this directory is a
+            # temporary way to get the params_file feature before it's been released
+            PythonLaunchDescriptionSource(PathJoinSubstitution(
+                [FindPackageShare('traini_gazebo'), 'launch', 'gazebo.launch.py'])),
             launch_arguments={
-                'world': PathJoinSubstitution([FindPackageShare('traini_gazebo'),'worlds','training_world.sdf']),
+                'world': PathJoinSubstitution([FindPackageShare('traini_gazebo'),
+                                               'worlds', 'training_world.sdf']),
                 'verbose': LaunchConfiguration('verbose'),
                 'gui': LaunchConfiguration('gui'),
                 'gui_required': 'true',
                 'server_required': 'false',
                 'factory': 'false',
-                'params_file': PathJoinSubstitution([FindPackageShare('traini_gazebo'),'config','gazebo_params.yaml']),
+                'params_file': PathJoinSubstitution([FindPackageShare('traini_gazebo'),
+                                                     'config', 'gazebo_params.yaml']),
             }.items()
         )
     ])
