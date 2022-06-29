a71d5306a414e07b1fde4516120201bbb4f22dc7
Matthew Barulic <matthew.barulic@gmail.com>
2022-06-28 20:26:34 -0400

    Fixes/updates a few things in gazebo launch setup.

--- traini_gazebo/launch/traini_gazebo.launch.py
+++ traini_gazebo/launch/traini_gazebo.launch.py
@@ -21,52 +21,34 @@
 import os
 import launch
 from launch import LaunchDescription
+from launch.event_handlers import OnShutdown
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import (IncludeLaunchDescription, SetEnvironmentVariable, RegisterEventHandler,
-                            ExecuteProcess)
+from launch.actions import (AppendEnvironmentVariable, DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable,
+                            RegisterEventHandler, ExecuteProcess)
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare, FindPackagePrefix
 from ament_index_python.packages import get_package_share_directory
-from ament_index_python.packages import get_package_prefix
 
 
 def generate_launch_description():
-    world_path = os.sep.join([get_package_share_directory(
-        'traini_gazebo'), 'worlds', 'training_world.sdf'])
-    gazebo_launch_path = os.sep.join(
-        [get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'])
-
-    traini_gazebo_model_path = os.sep.join(
-        [get_package_share_directory('traini_gazebo'), 'models'])
-    gazebo_model_path = os.pathsep.join(
-        [traini_gazebo_model_path, os.getenv('GAZEBO_MODEL_PATH', '')])
-
-    stsl_gazebo_plugin_path = os.sep.join(
-        [get_package_prefix('stsl_gazebo_plugins'), 'lib'])
-    gazebo_plugin_path = os.pathsep.join(
-        [stsl_gazebo_plugin_path, os.getenv('GAZEBO_PLUGIN_PATH', '')])
-
-    kill_server_script_path = os.sep.join(
-        [get_package_prefix('traini_gazebo'), 'bin', 'kill_gazebo_server.sh'])
-
     return LaunchDescription([
-        SetEnvironmentVariable('GAZEBO_PLUGIN_PATH', gazebo_plugin_path),
-        SetEnvironmentVariable('GAZEBO_MODEL_PATH', gazebo_model_path),
+        AppendEnvironmentVariable('GAZEBO_MODEL_PATH', PathJoinSubstitution([FindPackageShare('traini_gazebo'), 'models'])),
+        AppendEnvironmentVariable('GAZEBO_PLUGIN_PATH', PathJoinSubstitution([FindPackagePrefix('stsl_gazebo_plugins'), 'lib'])),
         # Disable trying to connect to online model database
         SetEnvironmentVariable('GAZEBO_MODEL_DATABASE_URI', ''),
+        DeclareLaunchArgument('verbose', default_value='false', choices=['false','true']),
+        DeclareLaunchArgument('gui', default_value='true', choices=['false','true']),
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gazebo_launch_path),
+            # TODO(barulicm) Copying the gazebo_ros launch files into this directory is a temporary way to get the params_file feature before it's been released
+            PythonLaunchDescriptionSource(PathJoinSubstitution([FindPackageShare('traini_gazebo'),'launch','gazebo.launch.py'])),
             launch_arguments={
-                'world': world_path,
-                'verbose': 'false',
+                'world': PathJoinSubstitution([FindPackageShare('traini_gazebo'),'worlds','training_world.sdf']),
+                'verbose': LaunchConfiguration('verbose'),
+                'gui': LaunchConfiguration('gui'),
                 'gui_required': 'true',
                 'server_required': 'false',
-                'factory': 'false'
+                'factory': 'false',
+                'params_file': PathJoinSubstitution([FindPackageShare('traini_gazebo'),'config','gazebo_params.yaml']),
             }.items()
-        ),
-        RegisterEventHandler(
-            launch.event_handlers.OnShutdown(
-                # This is a workaround for this issue:
-                # https://github.com/ros-simulation/gazebo_ros_pkgs/issues/1198
-                on_shutdown=[ExecuteProcess(cmd=[kill_server_script_path])]
-            )
         )
     ])
